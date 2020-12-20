ad679e3b5e885d561ca483824badaf2c9b35fd5c
arthur.gallina@gmail.com <arthur.gallina@gmail.com>
2020-12-20 19:06:24 -0300

    fix links

--- pages/productlist.js
+++ pages/productlist.js
@@ -3,9 +3,15 @@ import Link from "next/link";
 export default function Home({ products }) {
     return (
         <div>
-            {products.map((product) => (
+            {products?.map((product) => (
                 <Link href={`products/${product.price}`}>
-                    <h1 key={product.id}>
+                    <h1
+                        key={product.id}
+                        style={{
+                            cursor: "pointer",
+                            borderBottom: "1px solid black",
+                        }}
+                    >
                         {product.name} - {product.price}
                     </h1>
                 </Link>
@@ -15,8 +21,9 @@ export default function Home({ products }) {
 }
 
 export const getStaticProps = async () => {
-    const response = await fetch("https://backendtalkai.tech/products/");
-    const data = await response.json();
+    const response = await fetch("https://backendtalkai.tech/products");
+    const { data } = await response.json();
+
     return {
         props: {
             products: data,
